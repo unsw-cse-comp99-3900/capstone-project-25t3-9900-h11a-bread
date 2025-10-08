5ca32bf5a7134dcf7a3c387d61db6af7f2dc2174
Renzo Timothy Tan <renzotimtan@gmail.com>
2025-10-08 19:39:55 +1100

    config: setup tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>SystemX - React + TypeScript + Docker</h1>
+      <h1 className="text-blue-500">SystemX - React + TypeScript + Docker + Tailwind</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
